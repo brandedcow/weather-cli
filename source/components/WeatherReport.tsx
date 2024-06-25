24a7d5abaf4197ee0f3cd7555305b4583ed42916
Chris Kang <chris.v.kang@gmail.com>
2024-06-24 17:26:51 -0700

    feat: add chinese translation for current weather report

--- source/components/WeatherReport.tsx
+++ source/components/WeatherReport.tsx
@@ -1,18 +1,19 @@
 import React, {useEffect} from 'react';
 import {format} from 'date-fns';
 import {Box, Text} from 'ink';
-import {t} from 'i18next';
 import {Error} from './Error.js';
 import {useQuery} from '@tanstack/react-query';
 import {API} from '../api/openweather.js';
 import {Spinner} from '@inkjs/ui';
+import {config} from '../config.js';
+import {t} from 'i18next';
 
 type Props = {
 	city: GeocodeData;
 };
 
 export function WeatherReport({city}: Props) {
-	const {lat, lon, name, country, state} = city;
+	const {lat, lon, name, country, state, local_names} = city;
 	const {
 		isPending,
 		data: weatherData,
@@ -22,6 +23,8 @@ export function WeatherReport({city}: Props) {
 		queryKey: ['weatherData', lat, lon],
 		queryFn: () => API.onecall(lat, lon),
 	});
+
+	const currentLocale = config.get('locale');
 
 	useEffect(() => {
 		const fetchInterval = setInterval(() => {
@@ -52,10 +55,16 @@ export function WeatherReport({city}: Props) {
 			borderColor="cyan"
 			gap={1}
 		>
-			<Text>{t('weather_report.title')}</Text>
+			<Text>{t('weather_report.Title')}</Text>
 			<Box flexDirection="column">
-				<Text>{`${name}, ` + (state ? state + ', ' : '') + country}</Text>
-				<Text>Time: ({format(new Date(current.dt * 1000), 'P | p')})</Text>
+				<Text>
+					{`${local_names[currentLocale] ? local_names[currentLocale] : name}`}
+				</Text>
+				<Text>{`${(state ? state + ', ' : '') + country}`}</Text>
+				<Text>
+					{t('weather_report.Time')}: (
+					{format(new Date(current.dt * 1000), 'P | p')})
+				</Text>
 			</Box>
 			<Box flexDirection="column">
 				{current.weather.length > 0 && (
@@ -63,20 +72,27 @@ export function WeatherReport({city}: Props) {
 						{current.weather[0]?.main}: {current.weather[0]?.description}
 					</Text>
 				)}
-				<Text>Sunrise: {format(new Date(current.sunrise * 1000), 'p')}</Text>
-				<Text>Sunset: {format(new Date(current.sunset * 1000), 'p')}</Text>
+				<Text>
+					{t('weather_report.Sunrise')}:{' '}
+					{format(new Date(current.sunrise * 1000), 'p')}
+				</Text>
+				<Text>
+					{t('weather_report.Sunset')}:{' '}
+					{format(new Date(current.sunset * 1000), 'p')}
+				</Text>
 			</Box>
 
 			<Box flexDirection="column">
 				<Text>
-					Temperature: {current.temp} F (feels like {current.feels_like} F)
+					{t('weather_report.Temperature')}: {current.temp} F (feels like{' '}
+					{current.feels_like} F)
 				</Text>
-				<Text>Humidity: {current.humidity}%</Text>
-				<Text>Wind: {current.wind_speed} mph</Text>
-			</Box>
-
-			<Box flexDirection="column">
-				<Text></Text>
+				<Text>
+					{t('weather_report.Humidity')}: {current.humidity}%
+				</Text>
+				<Text>
+					{t('weather_report.Wind')}: {current.wind_speed} mph
+				</Text>
 			</Box>
 		</Box>
 	);
